76929ef87dc8a01b42db038f477a0aa49a8f1266
wertrain <wertrain@gmail.com>
2017-01-31 15:38:59 +0900

    gulpfileの追加

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    eslint = require('gulp-eslint');
+
+gulp.task('webserver', function() {
+    gulp.src('app')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('eslint', function() {
+    return gulp.src(['app/js/*.js'])
+        .pipe(eslint({ useEslintrc: true }))
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('default', ['webserver']);
