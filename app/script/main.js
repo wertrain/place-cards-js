f965e5b78877908aadc2d42503f3f1105f4af3c8
wertrain <wertrain@gmail.com>
2017-02-11 19:47:30 +0900

    スクリプト化した画像を追加して表示

--- app/script/main.js
+++ app/script/main.js
@@ -9,20 +9,30 @@
   canvas.width = CANVAS_WIDTH;
   canvas.height = CANVAS_HEIGHT;
 
+  let deck = new placecards.Deck();
+  deck.shuffle();
+  
+  let cardImages = [];
+  (function() {
+    let suitChar = ['s', 'd', 'h', 'c'];
+    for (let i = 0; i < suitChar.length; ++i) {
+      for (let j = 0; j < placecards.Deck.NUMBER_MAX; ++j) {
+        let image = new Image();
+        let num = j + 1;
+        image.src = eval("card_" + suitChar[i] + (num <= 9 ? "0" + num : num))
+        cardImages.push(image)
+      }
+    }
+  })();
+  let cardToImageIndex = function(card) {
+    return (card.suit - 1) * placecards.Deck.NUMBER_MAX + card.num;
+  }
+  
   let context = canvas.getContext('2d');
   (function() {
     context.fillStyle = 'rgba(0, 0, 0, 255)';
     context.fillRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(cardImages[0], 0, 0, 100, 150);
     //setTimeout(arguments.callee, 1000 / 60);
   })();
-
-  let deck = new placecards.Deck();
-  deck.shuffle();
-  
-  let cardToImageName = function(card) {
-    let suitChar = ['', 's', 'd', 'h', 'c'];
-    return suitChar[card.suit] + (('00' + (card.num + 1)).substr(-2)) + '.png';
-  };
-  let card = deck.popCard();
-  console.log(cardToImageName(card));
 }
