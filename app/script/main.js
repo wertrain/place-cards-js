74dde339199ef2a19ed92e84c65e6b4daa7850ab
wertrain <wertrain@gmail.com>
2017-02-17 10:58:22 +0900

    カードを順番に並べるようにする

--- app/script/main.js
+++ app/script/main.js
@@ -1,8 +1,13 @@
 'use strict';
 
 {
-  let CANVAS_WIDTH = 640;
-  let CANVAS_HEIGHT = 640;
+  let DRAW_OFFSET = 32;
+  let CARD_IMAGE_WIDTH = 75;
+  let CARD_IMAGE_HEIGHT = 125;
+  let X_LINE = 4, Y_LINE = 14;
+  
+  let CANVAS_WIDTH = CARD_IMAGE_WIDTH * X_LINE + DRAW_OFFSET * 2;
+  let CANVAS_HEIGHT = CARD_IMAGE_HEIGHT * Y_LINE + DRAW_OFFSET * 2 ;
   let CANVAS_ID = 'canvas';
   
   let canvas = document.getElementById(CANVAS_ID);
@@ -19,7 +24,7 @@
       for (let j = 0; j < placecards.Deck.NUMBER_MAX; ++j) {
         let image = new Image();
         let num = j + 1;
-        image.src = eval("card_" + suitChar[i] + (num <= 9 ? "0" + num : num))
+        image.src = eval("card_" + suitChar[i] + (num <= 9 ? "0" + num : num));
         cardImages.push(image)
       }
     }
@@ -33,17 +38,18 @@
   let placedCard = [];
 
   let context = canvas.getContext('2d');
-  (function() {
+  setInterval(function() {
     context.fillStyle = 'rgba(0, 0, 0, 255)';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    for (let y = 0; y < 4; ++y) {
-      for (let x = 0; x < 13; ++x) {
-        let count = (y * 13 + x);
+    for (let y = 0; y < Y_LINE; ++y) {
+      for (let x = 0; x < X_LINE; ++x) {
+        let count = (y * X_LINE + x);
         if (cardCount > count) {
-          context.drawImage(
-            cardImages[cardToImageIndex(placedCard[count])], 
-            0, 0, 100, 150);
+          let cardImage = cardImages[cardToImageIndex(placedCard[count])];
+          context.drawImage(cardImage, 
+            DRAW_OFFSET + x * CARD_IMAGE_WIDTH, DRAW_OFFSET + y * CARD_IMAGE_HEIGHT, 
+            CARD_IMAGE_WIDTH, CARD_IMAGE_HEIGHT);
         } else {
           break;
         }
@@ -54,8 +60,8 @@
       if (typeof nextCard !== "undefined") {
         placedCard.push(nextCard);
         ++cardCount;
+        frameCount = 0;
       }
     }
-    //setTimeout(arguments.callee, 1000 / 60);
-  })();
+  }, 1000 / 60);
 }
